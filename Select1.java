63ff212a320b0e0419c5eb2dac8566db3889f8bf
Uday Sai K <101692002+udaysai7777@users.noreply.github.com>
2022-03-19 10:54:40 +0530

    Add files via upload

---
+++ Select1.java
@@ -0,0 +1,21 @@
+
+public class Select1 {
+
+	public static void main(String[] args) {
+		int a=14;
+		{
+			if(a%2==0&&(a>10&&a<20))
+				{
+				System.out.println("Uday");
+				}
+			else if(a%2!=0&&(a>10&&a<20))
+			{	
+				System.out.println("Sai");
+			}
+			else
+			{
+				System.out.println("jerry");
+				}
+}
+	}
+}
