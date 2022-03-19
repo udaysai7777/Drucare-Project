4a7dd25e386a02a7ee943a7527814c2c112a6174
Uday Sai K <101692002+udaysai7777@users.noreply.github.com>
2022-03-19 10:34:23 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,13 @@
+public class Increment 
+{
+	public static void main(String args[])
+	{
+	int a=20,b=30,c=40,d=50;
+	int e=a++ + ++b + ++c + d++;
+   System.out.println(a);
+   System.out.println(b);
+   System.out.println(c);
+   System.out.println(d);
+   System.out.println(e);
+	}
+	}
---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+
+public class WhileLoop {
+public static void main(String args[])
+{
+	boolean i=true;
+	while(i==true)
+	{
+		System.out.println("Hello Uday");
+		break;
+	}
+}
+}
+
