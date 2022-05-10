819890c9bcca820fa608d5e8fef23d8a8ba013f2
Uday Sai K <101692002+udaysai7777@users.noreply.github.com>
2022-05-10 12:14:44 +0530

    Add files via upload

---
+++ UserRating.java
@@ -0,0 +1,18 @@
+package io.drucare.ratingdataservice.Model;
+
+import java.util.List;
+
+public class UserRating {
+	private List<Rating> userRating;
+
+	public List<Rating> getUserRating() {
+		return userRating;
+	}
+
+	public void setUserRating(List<Rating> userRating) {
+		this.userRating = userRating;
+	}
+	
+	
+
+}
